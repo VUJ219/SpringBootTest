8cb2c78d4b3f18073d0e7a6d2b1a9fc9a106bd14
luka <lukavujcic11@gmail.com>
2023-01-06 15:40:06 +0100

    Now 2 test records are inserted to h2 database.

--- src/main/java/com/vuj/practice/PracticeApplication.java
+++ src/main/java/com/vuj/practice/PracticeApplication.java
@@ -1,13 +1,25 @@
 package com.vuj.practice;
 
+import com.vuj.practice.model.User;
+import com.vuj.practice.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(UserRepository userRepository) {
+		return args -> {
+			userRepository.save(new User("Janos10", "janos@gmail.com", "af34awresfdy"));
+			userRepository.save(new User("_Bela_", "bela@gmail.com", "be14ak1r41y"));
+		};
 	}
 
 }
